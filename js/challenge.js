2f6e47ac8f590e64562411a9ae8a37b200015c94
Aziza  iliasova <miacorporation25@gmail.com>
2021-06-17 13:46:01 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -49,39 +49,30 @@
      addCtr(-1);
    if (id === 'plus')
      addCtr(1);
-
    if (id === 'heart') {
      const num = count;
      liked.push(num);
-
      let li = document.querySelector(`#n${num}`);
      if (!li) {
        li = document.createElement('li');
        li.id = `n${num}`;
      }
-
      li.innerText = `${num} has been liked ${liked.filter(x => x == num).length} times`;
-
      likes.append(li);
    }
 
    if (id === 'pause') {
      stopIteration(iterId);
-
      changeDisabledNodeArray(true, buttons);
      e.target.disabled = false;
-
      e.target.id = 'resume';
      e.target.value = 'resume';
      e.target.innerText = 'resume';
-
    }
 
    if (id === 'resume') {
      iterId = startIteration();
-
      changeDisabledNodeArray(false, buttons);
-
      e.target.id = 'pause';
      e.target.value = 'pause';
      e.target.innerText = 'pause';
